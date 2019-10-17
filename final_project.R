ef69cbae8f54c7f257e846f9bc8d423c83341ab0
Diego Machado <dmachado@go.olemiss.edu>
2019-10-17 02:09:25 -0500

    read files

--- final_project.R
+++ final_project.R
@@ -1,4 +1,12 @@
 #Final project
+#Diego Machado
+#Jake Tweedle
+#Isaiah Vaughnn
+
 
 #load libraries
 library(tidyverse)
+
+#read data files in
+cardio <- read_delim("data/cardio_train.csv", delim= ";")
+vital <- read_csv("data/Data1.csv")
