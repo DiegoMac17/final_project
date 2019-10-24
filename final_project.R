75007ed5f8cf0914c18166bcc68a3025bdac9c3d
jtweedle <jtweedle@go.olemiss.edu>
2019-10-24 14:05:37 -0500

    Hey

--- final_project.R
+++ final_project.R
@@ -10,3 +10,4 @@ library(tidyverse)
 #read data files in
 cardio <- read_delim("data/cardio_train.csv", delim= ";")
 vital <- read_csv("data/Data1.csv")
+vital1 <- spread(vital,Break_Out_Category,Break_Out)
